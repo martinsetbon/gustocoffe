2e867d29d0ab58e06136377dcdd35cb0c2344928
martin setbon <setbon.martin@gmail.com>
2025-03-18 05:45:29 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,66 @@
+        <div class="container-fluid" id="footer">
+            <footer>
+                <div class="row ">
+                    <div class="col-12">
+
+                        <div class="row mt-3">
+                            <div class="col-6 text-right">
+                                <button id="bouton-contraste"><i class="fas fa-low-vision"></i>&nbsp;&nbsp;Mode contrasté</button>
+                            </div>
+                            <div class="col-6" id="typo-accessibilite">
+                                <a href="#"><i class="fas fa-text-height typo-gros"></i></a>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-12 col-lg-3 border-right">
+                                <ul class="navigation-footer">
+                                    <li><a href="#">FAQ</a></li>
+                                    <li><a href="#">Mentions légales</a></li>
+                                    <li><a href="#">CGU</a></li>
+                                    <li><span style="color: white;">©</span></li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-lg-3 border-right">
+                                <ul class="navigation-footer">
+                                    <li> <a href="#">Reserver</a> </li>
+                                    <li> <a href="#">Nos espaces</a> </li>
+                                    <li><a href="#">Notre histoire</a></li>
+                                    <li> <a href="#">Notre carte</a> </li>
+                                    <li> <a href="#">Nos services</a> </li>
+                                    <li><a href="#">Nos partenaires</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-lg-3 border-right text-center">
+                                <div class="row mt-3">
+                                    <div class="col-6">
+                                        <a href="#"> <i class="fab fa-facebook-square reseau-blanc"></i></a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#"><i class="fab fa-twitter-square reseau-blanc"></i></a>
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-6">
+                                        <a href="#"> <i class="fab fa-linkedin reseau-blanc"></i></a>
+                                    </div>
+                                    <div class="col-6 text-center">
+                                        <a href="#"> <i class="fab fa-instagram reseau-blanc"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-lg-3 mt-3">
+                                <div id="contact" class="text-right">
+                                    <p>Gusto Coffe Corworking
+                                        28 rue Ambroise Paré
+                                        75010 Paris </p>
+                                    <a href="mailto:info@gusto-coffee.fr"><i class="far fa-envelope"></i>&nbsp;&nbsp;Gusto-coffe@gcc.com</a>
+                                    <p><i class="fas fa-mobile-alt"></i>&nbsp;&nbsp;01 31 78 93 77</p>
+                                    <p>Du Lundi au Vendredi: 6h - 22h
+                                        Samedi: 6h - minuit</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
---
+++ header.php
@@ -0,0 +1,94 @@
+        <div class="container-fluid" id="header-background">
+            <header>
+                <div class="row pt-3">
+                    <div class="col-12 text-center">
+                        <div class="reseau-header mx-auto">
+                            <a href="#"> <i class="fab fa-facebook-square facebook"></i></a>
+                            <a href="#"> <i class="fab fa-twitter-square twitter"></i></a>
+                            <a href="#"> <i class="fab fa-linkedin linkedin"></i></a>
+                            <a href="#"> <div class="instagram"></div> </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row ">
+                    <div class="col-12">
+                        <div class="recherche my-3">
+                            <form class="form-inline my-2 my-lg-0">
+                                <input id="input-recherche" class="form-control mr-sm-2" type="search" placeholder="tapez votre recherche" aria-label="Search">
+                                <button id="bouton-rechercher" class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div> 
+
+                <div class="row">
+                    <div class="col-3">
+                        <div>
+                            <a href="#"> <div id="logo"></div></a>
+                        </div>
+                    </div>
+
+                    <div class="col-6 align-center">
+                        <nav id="menu-principal" class="navbar navbar-expand-lg navbar-light bg-light">
+
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Reserver
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">Par espace</a>
+                                            <a class="dropdown-item" href="#">Par date</a>
+                                        </div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Nos espaces</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Notre histoire</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Notre carte</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Nos services</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Nos partenaires</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i id="langues" class="fas fa-globe"></i>
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">English</a>
+                                            <a class="dropdown-item" href="#">Italian</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+
+                    </div>
+
+                    <div class="col-3 text-right float-right">
+
+                        <a href="#"> <i class="fas fa-user-circle bouton-espace-membre-mobile"></i></a>
+
+                        <button class="boutons-accueil bouton-espace-membre">
+                            <a href="#">Mon compte</a>
+                        </button>
+                    </div>
+                </div>
+
+            </header>
+        </div>
